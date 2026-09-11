3f5a9f9842bfaf60971b269cfcb4a3914940909f
VeeraBabu Jakkula <squadpanda447@gmail.com>
2026-09-11 18:51:39 +0530

    Convert currency from USD to INR

---
+++ scratch.js
@@ -0,0 +1,3 @@
+console.log(new Intl.NumberFormat('en-IN', { style: 'currency', currency: 'INR', notation: 'compact', maximumFractionDigits: 1 }).format(150000 * 83.5));
+console.log(new Intl.NumberFormat('en-IN', { style: 'currency', currency: 'INR', notation: 'compact', maximumFractionDigits: 1 }).format(100000 * 83.5));
+console.log(new Intl.NumberFormat('en-IN', { style: 'currency', currency: 'INR', notation: 'compact', maximumFractionDigits: 1 }).format(200000 * 83.5));
